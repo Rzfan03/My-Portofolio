81eda267aee4fe3f26ed5238e9ea42ac1a0d1098
rzfann <fajrulsyaban47@gmail.com>
2025-12-14 13:52:10 +0700

    fix error at build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify/server";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: "server"
+  output: "server",
+  adapter: netlify()
 });
